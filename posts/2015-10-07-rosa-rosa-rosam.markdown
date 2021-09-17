8dbbc55c22f5b40ab839510853e23b263739b92a
Marc-André Brochu <marcandrebrochu@fastmail.com>
2021-09-18 00:57:04 +0200

    Begin implementing theme

--- posts/2015-10-07-rosa-rosa-rosam.markdown
+++ posts/2015-10-07-rosa-rosa-rosam.markdown
@@ -5,6 +5,9 @@ author: Ovidius
 
 Suspendisse pharetra ullamcorper sem et auctor. Suspendisse vitae tellus eu
 turpis dignissim gravida ut ut tortor. Cum sociis natoque penatibus et magnis
+
+<!--more-->
+
 dis parturient montes, nascetur ridiculus mus. Morbi aliquam sapien quis nisl
 sodales non aliquet nisl iaculis. Curabitur fermentum orci vel sapien
 pellentesque id condimentum metus vehicula. Curabitur turpis purus, scelerisque
--- site.hs
+++ site.hs
@@ -1,6 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+
+import Data.Monoid ((<>))
+import Hakyll
+import Hakyll.Core.Compiler.Internal (compilerUnsafeIO)
+import Text.Pandoc.Highlighting (Style, breezeDark, styleToCss)
+import Text.Pandoc.Options (ReaderOptions(..), WriterOptions(..))
+
+pandocCodeStyle :: Style
+pandocCodeStyle = breezeDark
 
 config :: Configuration
 config = defaultConfiguration
@@ -10,23 +17,29 @@ config = defaultConfiguration
 main :: IO ()
 main = hakyllWith config $ do
     match "images/*" $ do
-        route   idRoute
+        route idRoute
         compile copyFileCompiler
 
     match "css/*" $ do
-        route   idRoute
+        route idRoute
         compile compressCssCompiler
 
+    create ["css/syntax.css"] $ do
+        route idRoute
+        compile $ do
+            makeItem $ styleToCss pandocCodeStyle
+
     match (fromList ["about.rst", "contact.markdown"]) $ do
-        route   $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+        route $ setExtension "html"
+        compile $ pandocCompiler'
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext'
             >>= relativizeUrls
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+        compile $ pandocCompiler'
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/post.html" postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
@@ -35,23 +48,26 @@ main = hakyllWith config $ do
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "Archives" <>
+                    defaultContext'
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-
     match "index.html" $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    defaultContext
+            let firstPosts = take 2 posts
+                listCtx =
+                    teaserField "excerpt" "content" <>
+                    postCtx
+                indexCtx =
+                    listField "posts" listCtx (return firstPosts) <>
+                    defaultContext'
 
             getResourceBody
                 >>= applyAsTemplate indexCtx
@@ -60,9 +76,24 @@ main = hakyllWith config $ do
 
     match "templates/*" $ compile templateBodyCompiler
 
+defaultContext' :: Context String
+defaultContext' =
+    functionField "include" includeFiles <>
+    defaultContext
 
---------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+    dateField "date" "%B %e, %Y" <>
+    defaultContext'
+
+pandocCompiler' :: Compiler (Item String)
+pandocCompiler' = pandocCompilerWith
+    defaultHakyllReaderOptions
+    defaultHakyllWriterOptions
+        { writerHighlightStyle = Just pandocCodeStyle
+        }
+
+includeFiles :: [String] -> Item a -> Compiler String
+includeFiles files _ = compilerUnsafeIO $ do
+    contents <- sequence $ fmap readFile files
+    return $ concat contents
